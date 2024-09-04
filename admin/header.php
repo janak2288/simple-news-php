86245f09c5f32069740a779faa803e77c3e0d429
Janak Kafle <161906329+janak2288@users.noreply.github.com>
2024-09-04 06:41:19 +0545

    header footer added

---
+++ admin/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMC News Admin</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+     <style type="text/css">
+        body{
+              font-family: "Barlow", system-ui;
+
+        }
+    </style>
+</head>
+<body>
+<div class="flex"> 
+<div class="w-56 bg-gray-100 h-screen shadow"> 
+ <img src="https://smc.edu.np/wp-content/uploads/2023/11/smc-logo-circle.png" alt="" class="h-24 m-auto">
+<ul class="mt-5">
+<li class="my-2">
+<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-gray-200 rounded"> Home  </a> </li>
+<li class="my-2">
+<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-gray-200 rounded"> Catogories  </a> </li>
+<li class="my-2">
+<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-gray-200 rounded"> Notification  </a> </li>
+<li class="my-2">
+<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-gray-200 rounded"> Logout  </a> </li>
+ </ul>
+</div>
+<div class="p-4 flex-1">
--- header.php
+++ header.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <style type="text/css">
+        body{
+              font-family: "Barlow", system-ui;
+
+        }
+    </style>
 </head>
 <body>
     <nav class="flex items-center justify-between px-12 bg-slate-100">
