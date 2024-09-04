f65160a8561a7b9ab34442a890f85dd65ff9bfe3
Janak Kafle <161906329+janak2288@users.noreply.github.com>
2024-09-04 06:59:50 +0545

    added card in dashboard

--- admin/header.php
+++ admin/header.php
@@ -6,6 +6,8 @@
     <title>SMC News Admin</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/@icon/dashicons/dashicons.css">
+
      <style type="text/css">
         body{
               font-family: "Barlow", system-ui;
@@ -15,17 +17,17 @@
 </head>
 <body>
 <div class="flex"> 
-<div class="w-56 bg-gray-100 h-screen shadow"> 
+<div class="w-56 bg-indigo-100 h-screen shadow"> 
  <img src="https://smc.edu.np/wp-content/uploads/2023/11/smc-logo-circle.png" alt="" class="h-24 m-auto">
 <ul class="mt-5">
-<li class="my-2">
-<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-gray-200 rounded"> Home  </a> </li>
-<li class="my-2">
-<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-gray-200 rounded"> Catogories  </a> </li>
-<li class="my-2">
-<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-gray-200 rounded"> Notification  </a> </li>
-<li class="my-2">
-<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-gray-200 rounded"> Logout  </a> </li>
+<li class="my-2 text-center">
+<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-indigo-200 rounded text-indigo-600"> Home  </a> </li>
+<li class="my-2 text-center">
+<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-indigo-200 rounded text-indigo-600"> Catogories  </a> </li>
+<li class="my-2 text-center">
+<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-indigo-200 rounded text-indigo-600"> Notification  </a> </li>
+<li class="my-2 text-center">
+<a href="index.php" class="block p-2 text-lg font-bold  hover:bg-indigo-200 rounded text-indigo-600"> Logout  </a> </li>
  </ul>
 </div>
 <div class="p-4 flex-1">
--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,49 @@
 
 
 <h1 class="text-xl font-bold">Dashboard </h1>
+<hr class="h-1 bg-indigo-100">
+
+<div class="grid grid-cols-3 gap-5 mt-1"> 
+    <div class="bg-indigo-100 p-5 rounded-lg shadow text-center hover:bg-red-100 hover:text-indigo-600 transition duration-300">
+        <div class="flex items-center justify-center">
+            <div class="text-6xl text-indigo-600 mr-4">
+                <span class="dashicons dashicons-category"></span>
+            </div>
+            <div>
+                <h2 class="font-bold text-2xl">Categories</h2>
+                <p class="mt-2 text-4xl font-bold">4</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-indigo-100 p-5 rounded-lg shadow text-center hover:bg-red-100 hover:text-indigo-600 transition duration-300">
+        <div class="flex items-center justify-center">
+            <div class="text-6xl text-indigo-600 mr-4">
+                <span class="dashicons dashicons-admin-site-alt3"></span>
+            </div>
+            <div>
+                <h2 class="font-bold text-2xl">News</h2>
+                <p class="mt-2 text-4xl font-bold">4</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-indigo-100 p-5 rounded-lg shadow text-center hover:bg-red-100 hover:text-indigo-600 transition duration-300">
+        <div class="flex items-center justify-center">
+            <div class="text-6xl text-indigo-600 mr-4">
+                <span class="dashicons dashicons-visibility"></span>
+            </div>
+            <div>
+                <h2 class="font-bold text-2xl">Views</h2>
+                <p class="mt-2 text-4xl font-bold">4</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
 
 
 
